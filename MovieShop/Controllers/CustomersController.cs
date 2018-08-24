74cd5c4ad67e4df623f88806525e5d5ad64eaa76
phankhoi654321 <phankhoi654321@gmail.com>
2018-08-24 11:03:48 +0700

    Exercise 02 AspDotNet

---
+++ MovieShop/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieShop.Models;
+
+namespace MovieShop.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Mai Van Khanh"},
+                new Customer {Id = 2, Name = "Tran Van Tuan"}
+            };
+        }
+    }
+}
---
+++ MovieShop/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieShop.Models;
+
+namespace MovieShop.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Avenger 4"},
+                new Movie {Id = 2, Name = "Mission Impossible"}
+            };
+        }
+    }
+}
